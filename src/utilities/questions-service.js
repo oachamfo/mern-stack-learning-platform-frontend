63239f95fdd0f9920a524e455e8a956a03bc033f
“Owusu <owusuachamfour@gmail.com>
2023-10-10 17:59:14 -0400

    added absolute url to getQuestions() instead of relative url

--- src/utilities/questions-service.js
+++ src/utilities/questions-service.js
@@ -2,7 +2,11 @@ import axios from "axios";
 export async function getQuestions() {
   //fetch data
   const response = await fetch(
-    "https://mern-stack-questionandanswer-backend.onrender.com/questions"
+    "https://mern-stack-questionandanswer-backend.onrender.com/questions/",
+    {
+      method: "HEAD",
+      mode: "no-cors",
+    }
   );
   //parse data from json format to JS object
   let questions = await response.json();
